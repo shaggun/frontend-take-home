3a1536e4e5309f0182c39b987ac1d7e6b4c1178b
shaggun <shaggynetworks@gmail.com>
2025-05-05 15:23:21 -0600

    Update app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Theme, Container, Tabs, Box } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 import './App.css';
+import UsersTab from './components/Users/UsersTab';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastProvider } from './context/ToastContext';
+
+// Create a client for React Query
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <Theme >
+          <Container size="3" className="App">
+            <Box py="5">
+              <Tabs.Root defaultValue="users">
+                <Tabs.List>
+                  <Tabs.Trigger value="users">Users</Tabs.Trigger>
+                  <Tabs.Trigger value="roles">Roles</Tabs.Trigger>
+                </Tabs.List>
+                <Box pt="4">
+                  <Tabs.Content value="users">
+                    <UsersTab />
+                  </Tabs.Content>
+                  <Tabs.Content value="roles">
+
+                  </Tabs.Content>
+                </Box>
+              </Tabs.Root>
+            </Box>
+          </Container>
+        </Theme>
+      </ToastProvider>
+    </QueryClientProvider>
   );
 }
 
--- client/src/hooks/intex.ts
+++
@@ -1,2 +0,0 @@
-export { useDebounce } from './useDebounce';
-export { usePagination } from './usePagination';
