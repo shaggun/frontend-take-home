2c307f21d5e1a6769f09205eed7ae6716bfa37a1
shaggun <shaggynetworks@gmail.com>
2025-05-05 13:08:08 -0600

    Define types

---
+++ client/src/types/index.ts
@@ -0,0 +1,20 @@
+// User model
+export interface User {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  first: string;
+  last: string;
+  roleId: string;
+  photo?: string;
+}
+
+// Role model
+export interface Role {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  description?: string;
+  isDefault: boolean;
+}
